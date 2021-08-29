10d6f3f0b5fb3833c122a2d7bd9664a0ccd26cbf
Susheel Kumar <susheelkumartgap@gmail.com>
2021-08-29 21:00:56 +0530

    creating user schema, model and validating user input using express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-//@route GET api/users
-//@desc Test route
+//@route POST api/users
+//@desc  Register User route
 //@access public
-router.get('/', (req, res) => res.send('User Route'));
+router.post(
+'/',
+[
+  check('name', 'Name is Required')
+    .not()
+    .isEmpty(),
+   check('email', 'please enter a valid email').isEmail(),
+   check('password', 'please enter a password with 6 or more characters').isLength({ min:6 })
+],
+(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User Registered Successfully');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect DataBase
 connectDB();
 
+//Init MiddleWare to get request body
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
