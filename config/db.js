806b0adede48665b46d7c39f589963679b88d4a0
Susheel Kumar <susheelkumartgap@gmail.com>
2021-08-30 22:11:43 +0530

    save user to DB and assign a token

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () =>
 {
     try{
       await mongoose.connect(db, {
-          useNewUrlParser : true 
+          useNewUrlParser : true, 
       });
 
       console.log('MongoDB Connected...');
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
+const e = require('express');
 
 //@route POST api/users
 //@desc  Register User route
@@ -14,13 +21,71 @@ router.post(
    check('email', 'please enter a valid email').isEmail(),
    check('password', 'please enter a password with 6 or more characters').isLength({ min:6 })
 ],
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty())
     {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User Registered Successfully');
+
+    const { name, email, password } = req.body;
+
+    try {
+
+      // See if user exists
+      let user = await User.findOne({email});
+
+      if(user)
+      {
+        return res.status(400).json({ errors: [{msg: 'User already Exists'}] });
+      }
+
+      // Get users avatar
+      const avatar = gravatar.url(email, {
+        s: '200', //image size 
+        r: 'pg', // rating 
+        d: 'mm' //default image
+      })
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      // Return JsonWebToken
+      jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        {expiresIn: 36000},
+        (err, token) => {
+          if(err) throw err;
+          res.json({token});
+        }
+        );
+
+      //res.send('User Registered Successfully');
+
+    } catch( err )
+    {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+
 });
+
 
 module.exports = router;
