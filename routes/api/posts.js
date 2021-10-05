878909f6b323a6af01dd2ef67bea3d421aa11470
Susheel Kumar <susheelkumartgap@gmail.com>
2021-10-05 22:58:38 +0530

    Add Comment to a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -116,7 +116,7 @@ router.delete('/:id', auth, async (req, res) =>{
 
 
 
-//@route GET api/posts/like/:id
+//@route PUT api/posts/like/:id
 //@desc Like  a Post
 //@access private
 router.put('/like/:id', auth, async (req, res) =>{
@@ -138,8 +138,8 @@ router.put('/like/:id', auth, async (req, res) =>{
 });
 
 
-//@route GET api/posts/unlike/:id
-//@desc Like  a Post
+//@route PUT api/posts/unlike/:id
+//@desc UnLike  a Post
 //@access private
 router.put('/unlike/:id', auth, async (req, res) =>{
     try {
@@ -162,5 +162,47 @@ router.put('/unlike/:id', auth, async (req, res) =>{
       res.status(500).send('Server Error');
     }
   });
+
+
+
+//@route POST api/posts/comment/:id
+//@desc Comment on a Post
+//@access private
+router.post('/comment/:id', 
+[
+    auth,
+    [
+        check('text', 'Text is Required').not().isEmpty()
+    ]
+ ], 
+ async (req, res) => {
+     const errors = validationResult(req);
+     if(!errors.isEmpty())
+     {
+         return res.status(400).json({ errors : errors.array() });
+     }
+     try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+
+        res.json(post.comments);
+         
+     } catch (err) {
+         console.error(err.message);
+         res.status(500).send('Server Error');
+     }
+});
+
 
 module.exports = router;
